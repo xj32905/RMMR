eab2068d8a8c1fb9254bdefd5f2e0c71699e48dd
xj32905 <3607639259@qq.com>
2026-05-17 22:44:01 +0800

    chore: 移除根目录下的 src（构建产物及不应提交的代码）

--- src/build/village_li/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/xj/town_ws/src/install/village_li'
--- src/village_li/village_li/li4.py
+++
@@ -1,9 +0,0 @@
-import rclpy
-from rclpy.node import Node
-
-def main(args=None):
-    rclpy.init(args=args)
-    li4_node = Node("li4")        # 创建节点，变量名是 li4_node
-    rclpy.spin(li4_node)          # 这里传变量 li4_node，不加引号！
-    rclpy.shutdown()
-
